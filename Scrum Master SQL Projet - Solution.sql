19b09a5f155578bdb99221ea9d8aaec5ea20b419
Xavi Velasco <contacto@xavivelasco.com>
2024-08-26 08:08:42 +0200

    Update Scrum Master SQL Projet - Solution.sql

--- Scrum Master SQL Projet - Solution.sql
+++ Scrum Master SQL Projet - Solution.sql
@@ -2,14 +2,14 @@
    Scrum Master's Questions
    ------------------------*/
 
--- 1. **Velocity**: _What is the team's average velocity (story points completed)?_
--- 2. **Peak Performance**: _When did the team achieve its best overall performance in terms of story points completed?_ 
--- 3. **Efficiency**: _How efficiently is the team utilizing its available hours?_
--- 4. **Planned vs. Unplanned Work**: _What is the ratio of planned story points completed to unplanned story points completed?_ 
--- 5. **Task Completion**: _What is the average completion rate of planned tasks?_
--- 6. **Unplanned Work**: _What percentage of the total sprint workload is taken up by unplanned work?_
--- 7. **Unplanned Issues**: _What is the average number of unplanned issues introduced per sprint?_ 
--- 8. **Story Refinement**: _Is the team improving stories refinement?_
--- 9. **Defects**: _What percentage of the total sprint workload is taken up by bugs?_
--- 10. **Commitment vs. Delivery**: _What is the percentage of story points committed that were successfully completed during the sprint?_ 
+-- 1. Velocity: What is the team's average velocity (story points completed)?
+-- 2. Peak Performance: When did the team achieve its best overall performance in terms of story points completed?
+-- 3. Efficiency: How efficiently is the team utilizing its available hours?
+-- 4. Planned vs. Unplanned Work: What is the ratio of planned story points completed to unplanned story points completed?
+-- 5. Task Completion: What is the average completion rate of planned tasks?
+-- 6. Unplanned Work: What percentage of the total sprint workload is taken up by unplanned work?
+-- 7. Unplanned Issues: What is the average number of unplanned issues introduced per sprint?
+-- 8. Story Refinement: Is the team improving stories refinement?
+-- 9. Defects: What percentage of the total sprint workload is taken up by bugs?
+-- 10. Commitment vs. Delivery: What is the percentage of story points committed that were successfully completed during the sprint?
 
